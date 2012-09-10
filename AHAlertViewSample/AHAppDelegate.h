e37df1c2cbad92a6137679341a90a3989a17675c
Warren Moore <wm@warrenmoore.net>
2012-09-10 12:02:33 -0500

    Initial Commit

---
+++ AHAlertViewSample/AHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AHAppDelegate.h
+//  AHAlertViewSample
+//
+//  Created by Warren Moore on 9/10/12.
+//  Copyright (c) 2012 Auerhaus Development, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AHAlertViewSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AHAlertViewSample
+//
+//  Created by Warren Moore on 9/10/12.
+//  Copyright (c) 2012 Auerhaus Development, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AHAppDelegate class]));
+	}
+}
